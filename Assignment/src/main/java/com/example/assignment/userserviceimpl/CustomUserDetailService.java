ea8fb8931d4db8f15b40aabddb2f566ec00751ba
Ishwor_007 <raayaseetal@gmail.com>
2023-07-20 14:47:30 +0545

    implementation of the service interface or the business logic

--- Assignment/src/main/java/com/example/assignment/userserviceimpl/CustomUserDetailService.java
+++ Assignment/src/main/java/com/example/assignment/userserviceimpl/CustomUserDetailService.java
@@ -1,8 +1,6 @@
 package com.example.assignment.userserviceimpl;
 
 
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
