74813a5a4dfa8dabe53ce63e54dff99d9edbaff9
Ishwor_007 <raayaseetal@gmail.com>
2023-07-20 14:46:06 +0545

    fetching and saving the details of user in database

--- Assignment/src/main/java/com/example/assignment/userrepository/UserRepo.java
+++ Assignment/src/main/java/com/example/assignment/userrepository/UserRepo.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.assignment.model.User;
 
 public interface UserRepo extends JpaRepository<User, Integer>{
-
 	
 	public User findByEmail(String email);
 
